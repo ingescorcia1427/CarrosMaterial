98dfcf9409476781ac791c0490841ff2ed4c15c4
Leonard <ingescorciapalmera@gmail.com>
2017-06-03 11:38:42 -0500

    Inicio de la app carros

---
+++ app/src/main/java/com/example/cuc/carros/AgregarCarro.java
@@ -0,0 +1,15 @@
+package com.example.cuc.carros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cuc.carros.R;
+
+public class AgregarCarro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_carro);
+    }
+}
---
+++ app/src/main/java/com/example/cuc/carros/Carro.java
@@ -0,0 +1,16 @@
+package com.example.cuc.carros;
+
+/**
+ * Created by CUC on 3/06/2017.
+ */
+
+public class Carro {
+    private String uuid;
+    private String urlfoto;
+    private String nchasis;
+    private String marca;
+    private String color;
+    private String modelo;
+    private String idfoto;
+
+}
